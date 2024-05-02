7ca9ff17acdc74287edeb7094e0c6a3f70d2fc1b
AfrodytaP <59373204+AfrodytaP@users.noreply.github.com>
2024-05-02 16:33:36 +0100

    Requirement 4 - Test 7

--- spec/Transaction.spec.js
+++ spec/Transaction.spec.js
@@ -17,7 +17,7 @@ describe("Transaction Class Tests", () => {
 
     //This is a suite of tests for requirements 4
     it("Requirement 4 - Test 3) should create a new instance of Transaction Class", () => {
-      //This is a test that checks if an instance of Transaction Class can be made
+      //This is a tests that checks if an instance of Transaction Class can be made
       //Arrange
       //Act
       //Assert
@@ -25,7 +25,7 @@ describe("Transaction Class Tests", () => {
     });
 
     it("Requirement 4 - Test 4) should call the getDate function of Transaction Class", () => {
-      //This is a test that checks if the getDate function of Transaction Class can be called
+      //This is a tests that checks if the getDate function of Transaction Class can be called
       //Arrange
       //Act
       spyOn(testTransaction, "getDate");
@@ -35,7 +35,7 @@ describe("Transaction Class Tests", () => {
     });
 
     it("Requirement 4 - Test 5) should call the getDate function of Transaction Class", () => {
-      //This is a test that checks if the getDate function returns the set date
+      //This is a tests that checks if the getDate function returns the set date
 
       //Arrange
       const expected = "10-01-2012";
@@ -45,13 +45,37 @@ describe("Transaction Class Tests", () => {
     });
 
     it("Requirement 4 - Test 6) should call the getDate function of Transaction Class", () => {
-      //This is a test that checks if the date is not set the current date will be used
+      //This is a tests that checks if the date is not set the current date will be used
       //Arrange
       const expected = new Date().toLocaleDateString();
       testTransactionEmpty = new Transaction();
       //Act
       //Assert
       expect(testTransactionEmpty.getDate()).toBe(expected);
+    });
+  });
+
+  describe("Testing the Credit aspect", () => {
+    // Will replace REPEATED arrange code
+    let testTransaction, testTransactionEmpty;
+
+    beforeEach(() => {
+      testTransaction = new Transaction("10-01-2012", 1000, 0, 1000);
+    });
+
+    afterEach(() => {
+      testTransaction = undefined;
+      testTransactionEmpty = undefined;
+    });
+
+    it("Requirement 4 - Test 7) should call the getCredit function of Transaction Class", () => {
+      //This is a tests that checks if the getCredit function of Transaction Class can be called
+      //Arrange
+      //Act
+      spyOn(testTransaction, "getCredit");
+      testTransaction.getCredit();
+      //Assert
+      expect(testTransaction.getCredit).toHaveBeenCalledWith();
     });
   });
 });
